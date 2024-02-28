bd5bee83e0f91618f19ef2710421d75c49f9e7b7
anthvanhorn <127164026+anthvanhorn@users.noreply.github.com>
2024-02-27 22:30:11 -0500

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2020caln",
+      "defaultValue": "2024miket",
       "required": "true"
     },
     { "name": "Match Level",
