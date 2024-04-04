772ca8eecf33987de1a4b751898b475bd0ffdf5e
anthvanhorn <127164026+anthvanhorn@users.noreply.github.com>
2024-04-03 23:35:33 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024mimtp"
+      "defaultValue": "2024micmp2"
     },
     { "name": "Match Level",
       "code": "l",
